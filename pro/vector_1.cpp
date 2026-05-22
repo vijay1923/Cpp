f2ff9aef3bea5ec846c2565b5b03a6150a8c3976
vijay1923 <vijaymagadum1923@gmail.com>
2026-05-22 10:53:43 +0530

    Initial commit

---
+++ pro/vector_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+
+int main()
+{
+    // cout<<"Welcome"<<endl;
+
+    vector<int>num;
+    int i;
+    num.push_back(0);
+
+    for (i=0;i<10;i++)
+    num.push_back(i);
+
+    
+    
+
+}
